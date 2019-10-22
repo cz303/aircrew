f67c9750212f26f822bdbed03b0db1c6a662befc
GENESIS <MSHRMV@GENESIS.local>
2019-10-22 19:10:03 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {telegraf_token:'951388162:AAEtCaLOXZMXrepJ4S0r09bKHNzdZQNxyuE',
+database:'mongodb://heroku_3w928lwx:44rmalubetp9d2f8mpb06iit51@ds337718.mlab.com:37718/heroku_3w928lwx',
+secret:'yoursecret'};
+
