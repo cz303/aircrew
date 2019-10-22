df0f8288dda225f3691a5bc3d21b99cb725d7cb6
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-22 21:51:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {telegraf_token:'951388162:AAEtCaLOXZMXrepJ4S0r09bKHNzdZQNxyuE',
 database:'mongodb://heroku_wwkpm46n:paivl4abvbof9tgs7tl8vcucgn@ds337718.mlab.com:37718/heroku_wwkpm46n',
 secret:'yoursecret'};
-
+app.listen(process.env.PORT || 3000);
