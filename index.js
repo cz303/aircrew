b6f563849f44126e03e292871629659707ebb63c
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-23 12:06:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const rateLimit = require('telegraf-ratelimit')
 var mongoose = require('mongoose');
 const User = require('./user');
 var ethereum_address = require('ethereum-address'); //used for verifying eth address
+app.set('port', (process.env.PORT || 5000));
 
 mongoose.connect(config.database, {
     socketTimeoutMS: 45000,
